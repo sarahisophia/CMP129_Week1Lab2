19fd9bfae613ed3bdf0747e79efaca6f80bac75f
sarahisophia <quinteroval.sarah@student.ccm.edu>
2026-09-08 13:38:36 -0400

    work for lab 2

--- SalesBarChart.java
+++ SalesBarChart.java
@@ -1,5 +1,32 @@
-package Labs.Week_1.scr;
-
-public class SalesBarChart {
-    
+import java.util.Scanner;
+public class SalesBarChart
+{
+    public static void main(String[] args) 
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int stores = 5;
+        int[] salesData = new int[stores]; 
+        int j = 0;
+        while (j < stores) 
+        {
+            System.out.print("Enter today's sale for store " + (j + 1) + ": ");
+            salesData[j] = keyboard.nextInt();
+            j++;
+        }
+        
+        System.out.println("\nSALES BAR CHART:");
+        System.out.println("Each * = $100");
+        
+        for (int i = 0; i < stores; i++) 
+        {
+            System.out.print("Store " + (i + 1) + ": ");
+            int asterisks = salesData[i]/100;
+            for (int k = 0; k < asterisks; k++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
